8ca5006a6905ade9e110ef471ae3c1dc04f82913
jorguardiola <jorguardiola@gmail.com>
2025-11-25 19:31:11 +0100

    subir frontprueba

---
+++ frontend/templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <title>Coffecommerce</title>
+    <link rel="stylesheet" href="../assets/css/style.css" />
+</head>
+<body>
+    <header>
+        <h1>Coffecommerce</h1>
+        <nav>
+            <a href="index.php">Inicio</a> |
+            <a href="search.php">Buscar</a> |
+            <a href="cart.php">Carrito</a> |
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
+    <main>
